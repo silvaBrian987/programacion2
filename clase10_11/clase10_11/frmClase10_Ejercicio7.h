5e2e44c63e7708f9a60eef5f596ba33821db709b
Brian Silva <silva.brian.987@gmail.com>
2016-11-22 23:46:05 -0300

    clase10_ejercicio7

--- clase10_11/clase10_11/frmClase10_Ejercicio7.h
+++ clase10_11/clase10_11/frmClase10_Ejercicio7.h
@@ -34,6 +34,22 @@ namespace clase10_11 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  btnCalcularPromedio;
+	protected:
+
+	private: System::Windows::Forms::TextBox^  txtAlumno;
+	protected:
+
+	protected:
+
+	private: System::Windows::Forms::ListBox^  lbNotas;
+
+	private: System::Windows::Forms::Button^  btnAgregarNota;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  btnDefinirAlumno;
+	private: System::Windows::Forms::GroupBox^  gbNotas;
+
+
 
 	private:
 		/// <summary>
@@ -48,18 +64,137 @@ namespace clase10_11 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->btnCalcularPromedio = (gcnew System::Windows::Forms::Button());
+			this->txtAlumno = (gcnew System::Windows::Forms::TextBox());
+			this->lbNotas = (gcnew System::Windows::Forms::ListBox());
+			this->btnAgregarNota = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->btnDefinirAlumno = (gcnew System::Windows::Forms::Button());
+			this->gbNotas = (gcnew System::Windows::Forms::GroupBox());
+			this->gbNotas->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// btnCalcularPromedio
+			// 
+			this->btnCalcularPromedio->Location = System::Drawing::Point(152, 64);
+			this->btnCalcularPromedio->Name = L"btnCalcularPromedio";
+			this->btnCalcularPromedio->Size = System::Drawing::Size(104, 23);
+			this->btnCalcularPromedio->TabIndex = 0;
+			this->btnCalcularPromedio->Text = L"Calcular promedio";
+			this->btnCalcularPromedio->UseVisualStyleBackColor = true;
+			this->btnCalcularPromedio->Click += gcnew System::EventHandler(this, &frmClase10_Ejercicio7::btnCalcularPromedio_Click);
+			// 
+			// txtAlumno
+			// 
+			this->txtAlumno->Location = System::Drawing::Point(72, 16);
+			this->txtAlumno->Name = L"txtAlumno";
+			this->txtAlumno->Size = System::Drawing::Size(120, 20);
+			this->txtAlumno->TabIndex = 1;
+			// 
+			// lbNotas
+			// 
+			this->lbNotas->FormattingEnabled = true;
+			this->lbNotas->Location = System::Drawing::Point(16, 32);
+			this->lbNotas->Name = L"lbNotas";
+			this->lbNotas->Size = System::Drawing::Size(120, 108);
+			this->lbNotas->TabIndex = 2;
+			// 
+			// btnAgregarNota
+			// 
+			this->btnAgregarNota->Location = System::Drawing::Point(152, 32);
+			this->btnAgregarNota->Name = L"btnAgregarNota";
+			this->btnAgregarNota->Size = System::Drawing::Size(104, 23);
+			this->btnAgregarNota->TabIndex = 3;
+			this->btnAgregarNota->Text = L"Agregar nota";
+			this->btnAgregarNota->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(16, 16);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(45, 13);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"Alumno:";
+			// 
+			// btnDefinirAlumno
+			// 
+			this->btnDefinirAlumno->Location = System::Drawing::Point(200, 16);
+			this->btnDefinirAlumno->Name = L"btnDefinirAlumno";
+			this->btnDefinirAlumno->Size = System::Drawing::Size(88, 23);
+			this->btnDefinirAlumno->TabIndex = 5;
+			this->btnDefinirAlumno->Text = L"Definir alumno";
+			this->btnDefinirAlumno->UseVisualStyleBackColor = true;
+			this->btnDefinirAlumno->Click += gcnew System::EventHandler(this, &frmClase10_Ejercicio7::btnDefinirAlumno_Click);
+			// 
+			// gbNotas
+			// 
+			this->gbNotas->Controls->Add(this->btnAgregarNota);
+			this->gbNotas->Controls->Add(this->lbNotas);
+			this->gbNotas->Controls->Add(this->btnCalcularPromedio);
+			this->gbNotas->Enabled = false;
+			this->gbNotas->Location = System::Drawing::Point(16, 48);
+			this->gbNotas->Name = L"gbNotas";
+			this->gbNotas->Size = System::Drawing::Size(272, 168);
+			this->gbNotas->TabIndex = 6;
+			this->gbNotas->TabStop = false;
+			this->gbNotas->Text = L"Notas";
 			// 
 			// frmClase10_Ejercicio7
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(387, 364);
+			this->ClientSize = System::Drawing::Size(303, 225);
+			this->Controls->Add(this->gbNotas);
+			this->Controls->Add(this->btnDefinirAlumno);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->txtAlumno);
 			this->Name = L"frmClase10_Ejercicio7";
 			this->Text = L"frmClase10_Ejercicio7";
+			this->gbNotas->ResumeLayout(false);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+		ref struct NotasAlumno{
+			String ^ Alumno;
+			System::Collections::Generic::List<Double> ^ Notas = gcnew System::Collections::Generic::List < Double > ;
+
+			Double ObtenerPromedio(){
+				if (Notas->Count > 0){
+					Double totalNotas = 0.0;
+					for (int i = 0; i < Notas->Count; i++){
+						totalNotas += Notas[i];
+					}
+					return totalNotas / Notas->Count;
+				}
+				else{
+					return 0.0;
+				}
+			}
+
+			String^ ObtenerEstadoSituacion(){
+				Double promedio = ObtenerPromedio();
+
+				if (promedio >= 7){
+					return "Coloquio";
+				}
+				else if (promedio < 7 && promedio >= 4){
+					return "Final";
+				}
+				else{
+					return "Desaprobado";
+				}
+			}
+		};
+
+		NotasAlumno alumno;
+	private: System::Void btnCalcularPromedio_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void btnDefinirAlumno_Click(System::Object^  sender, System::EventArgs^  e) {
+		alumno.Alumno = txtAlumno->Text;
+		
+	}
+};
 }
